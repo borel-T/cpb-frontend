68000b70ff15f6398c2e70fa5764f673983a4015
borel-T <borel-T@users.noreply.github.com>
2023-12-08 06:35:26 +0300

    modified api files

--- src/api/httpClient/endpoints.js
+++ src/api/httpClient/endpoints.js
@@ -1,2 +1,3 @@
 // api endpoints
-export const BASE_URL = "http://localhost:4000/";
+export const BASE_URL = "http://localhost:4000";
+export const PRODUCT_API = "/api/v1/products";
--- src/api/productService/index.js
+++ src/api/productService/index.js
@@ -1,7 +1,8 @@
 // products service
 import httpClient from "../httpClient";
+import { PRODUCT_API } from "../httpClient/endpoints";
 
 export default {
-  getById: (id) => httpClient.getRequest(`/api/products/${id}`),
-  getAll: () => httpClient.getRequest("/api/products"),
+  getById: (id) => httpClient.getRequest(`${PRODUCT_API}/${id}`),
+  getAll: () => httpClient.getRequest(`${PRODUCT_API}`),
 };
