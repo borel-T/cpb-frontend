57b20eb82b677d1402235adcdf413b4b33086d95
borel-T <borel-T@users.noreply.github.com>
2023-12-08 14:53:40 +0300

    added image-canvas component

--- src/components/imgCanvas/index.js
+++ src/components/imgCanvas/index.js
@@ -10,7 +10,32 @@ const ImageCanvas = ({ src = "" }) => {
 
     img.onload = () => {
       // Draw the image on the canvas once it's loaded
-      context.drawImage(img, 10, 10, 180, 180);
+      // context.drawImage(img, 10, 10, 180, 180);
+
+      // Clear the canvas and draw the image
+      context.clearRect(0, 0, canvas.width, canvas.height);
+
+      const maxWidth = 320;
+      const maxHeight = 200;
+
+      let width = img.width;
+      let height = img.height;
+
+      // Resize the image to fit within the specified dimensions
+      if (width > maxWidth) {
+        height *= maxWidth / width;
+        width = maxWidth;
+      }
+
+      if (height > maxHeight) {
+        width *= maxHeight / height;
+        height = maxHeight;
+      }
+
+      canvas.width = width;
+      canvas.height = height;
+
+      context.drawImage(img, 0, 0, width, height);
     };
 
     // Replace 'your-image-path.jpg' with the path to your image
